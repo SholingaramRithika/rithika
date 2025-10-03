005ca743cc2b5482fa1c55f56ddfbddfea2c03ac
Rithika Sholingaram <rithusholingaram@gmail.com>
2025-10-03 22:36:34 +0530

    Add files via upload

---
+++ address.py
@@ -0,0 +1,8 @@
+# Print Address 
+
+street = input("Enter your street name:")
+city = input("Enter your city name:")
+state = input("Enter your state name:")
+pin_code = input("Enter your pin code number:")
+
+print(f"Your address is:\n{street}\n{city}\n{state}\n{pin_code}")
